04bee3d972838d6d80bc75ee8d390eac45b962ed
Huey Kolowich <huey@Hughs-MacBook.local>
2021-01-26 14:07:42 -0700

    Lexer Class

---
+++ Lexer.h
@@ -0,0 +1,21 @@
+//Huey Kolowich
+//CS_236
+
+using namespace std;
+
+class Lexer 
+{
+private:
+    //vector<Token*> tokens;
+    //vector<Automaton*> automata;
+
+public:
+    Lexer::Lexer() 
+    {
+        //tokens = new std::vector<Token*>();
+        //automata = new std::vector<Automaton*>();
+        // Add all of the Automaton instances
+        //automata.push_back(new ColonAutomaton());
+        //automata.push_back(new ColonDashAutomaton());
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,12 @@
+//Huey Kolowich
+//CS 236
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) 
+{
+    //Testing for lexer
+    
+}
